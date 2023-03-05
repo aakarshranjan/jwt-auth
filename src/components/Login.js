b97b594a88168da165bb813b4c677f0acdecdbb1
Aakarsh Sinha <aakarshranjan9@gmail.com>
2023-03-05 22:03:54 +0530

    Project Setup

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Login = () => {
+    return(
+        <button className="text-3xl font-bold">Login</button>
+    )
+}
+
+export default Login
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+        hot: true
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            }
+        ]
+    }
+}
